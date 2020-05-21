fb83f950d7ea601de6dd96c4b980889f737f1734
palhamel <pal.hamel@gmail.com>
2020-05-21 15:40:58 +0200

    Add dependencies and setup mongodb and mongoose model

--- server.js
+++ server.js
@@ -1,24 +1,72 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from 'bcrypt-nodejs'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+
+// Mongoose & Database setup:
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/mongo-auth-codealong";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// -----------------------------------------------
+// Mongoose model setup:
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+
+
+
+
+
+
+
+
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+// supersecret endpoint
+// restrict access
+
+app.get("/secrets", (req, res) => {
+  res.json({ secret: "This is a super secret message" });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
